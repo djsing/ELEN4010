b2cfd204e771c496a7ab95df3406783f6657ab4b
Tyson <tyson.cross@gmail.com>
2019-04-10 12:26:20 +0200

    Basic hosting structure and routing

---
+++ index.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let app = express();
+let mainRouter = require('./app/routes/mainRoutes')
+app.use('/', mainRouter)
+let port_num = 3000
+app.listen(process.env.port || port_num)
+console.log('Express server running on port', port_num)
