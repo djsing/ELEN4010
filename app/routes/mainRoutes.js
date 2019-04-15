5d3b34c7a9d6e6cb92d0e9dec5e050cde2030472
Darrion Singh <1056673@students.wits.ac.za>
2019-04-15 23:08:56 +0200

    database dummy test replaced  username and password with real ones to check connection test query sucessfull, returns JSON file

--- app/routes/mainRoutes.js
+++ app/routes/mainRoutes.js
@@ -3,6 +3,7 @@
 let path = require('path')
 let express = require('express')
 let mainRouter = express.Router()
+let db = require('../models/db.js')
 
 mainRouter.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'index.html'))
@@ -10,6 +11,27 @@ mainRouter.get('/', function (req, res) {
 
 mainRouter.get('/about/', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'about.html'))
+})
+
+mainRouter.get('/database/', function (req, res) {
+  // Make a query to the database
+  db.pools
+    // Run query
+    .then((pool) => {
+      return pool.request()
+        // This is only a test query, change it to whatever you need
+        .query('SELECT 1')
+    })
+    // Send back the result
+    .then(result => {
+      res.send(result)
+    })
+    // If there's an error, return that with some description
+    .catch(err => {
+      res.send({
+        Error: err
+      })
+    })
 })
 
 mainRouter.get('*', function (req, res) {
