2791c1b16f307f2015255e1cf6dd861f3709d3d9
Thabang Mabula <1364103@students.wits.ac.za>
2019-04-20 00:08:31 +0200

    Auto stash before merge of "TermsAndConditions" and "origin/development"

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ let express = require('express')
 let path = require('path')
 let app = express()
 let mainRouter = require('./app/routes/mainRoutes')
-let termsAndConditionsController = require('./app/controllers/termsAndConditionsController')
 
 app.set('views', path.join(__dirname, './app/views'))
 app.set('view engine', 'html')
+
 app.use(express.static(path.join(__dirname, './app/public')))
 
 app.use('/', mainRouter)
+
 let port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
-
-termsAndConditionsController(app)
