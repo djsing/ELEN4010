8fff183bd850c40d7123eebdc9a6e23570c3d7eb
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-04-20 21:59:06 +0200

    Add trip title textbox on button click

--- app/public/js/trips.js
+++ app/public/js/trips.js
@@ -1,8 +1,17 @@
 let button = document.getElementById('addButton')
 
+// button.addEventListener('click', function () {
+//   let paragraph = document.createElement('p')
+//   let text = document.createTextNode('This is a trip')
+//   paragraph.appendChild(text)
+//   let trips = document.getElementById('tripsList')
+//   trips.appendChild(paragraph)
+// }, false)
+
 button.addEventListener('click', function () {
-  let paragraph = document.createElement('p')
-  let text = document.createTextNode('This is a trip')
-  paragraph.appendChild(text)
-  document.body.appendChild(paragraph)
+  let input = document.createElement('input')
+  input.type = 'text'
+  input.name = 'tripTitle[]'
+  let tripsList = document.getElementById('tripsList')
+  tripsList.appendChild(input)
 }, false)
