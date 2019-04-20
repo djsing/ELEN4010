f6892bd6345a93e48aaf4eddd08b7bbfb8184f48
Thabang Mabula <1364103@students.wits.ac.za>
2019-04-20 02:07:24 +0200

    Using EJS view for Terms and Conditions

--- app/controllers/termsAndConditionsController.js
+++ app/controllers/termsAndConditionsController.js
@@ -1,6 +1,20 @@
 'use strict'
 let path = require('path')
 
+var terms = ['Be nice', 'Be kind', 'Have fun']
+
 module.exports = function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'terms_and_conditions.html'))
+  res.render(path.join(__dirname, '../views', 'terms_and_conditions'), { termList: terms })
+  // res.sendFile(path.join(__dirname, '../views', 'terms_and_conditions.html'))
 }
+
+/*
+function displayTermsAndConditions () {
+  termsList.forEach(function (term) {
+    var text = document.createTextNode(term)
+    var listElement = document.createElement('LI')
+    listElement.appendChild(text)
+    document.getElementById('termsAndConditionsList').appendChild(listElement)
+  })
+}
+*/
