e83b6caa171f3ac5410253a35e6cb5d86c5aa667
Darrion Singh <1056673@students.wits.ac.za>
2019-04-20 11:46:11 +0200

    redirects to map (home) page after login success

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -9,6 +9,8 @@ function signInInit () {
       console.log('GoogleAuth object initialised.')
       // click handler
       let onLoginSuccess = function (googleUser) {
+        // redirect to home page, currently the map page
+        window.location = '/map'
         let profile = googleUser.getBasicProfile()
         console.log('ID: ' + profile.getId()) // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName())
