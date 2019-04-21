825c2ce6911ce77a6d735d71901c0187c66f871e
Thabang Mabula <1364103@students.wits.ac.za>
2019-04-21 15:02:17 +0200

    Set up a query to database for T&Cs

--- app/models/termsAndConditionsModel.js
+++ app/models/termsAndConditionsModel.js
@@ -1,1 +1,10 @@
-module.exports = ['Be nice', 'Be kind', 'Have fun']
+let db = require('./db.js')
+
+const getTermsAndConditions = function () {
+  // Get terms and conditions from databasez
+  var request = new db.sql.Request(db.pools)
+  request.query('select * from TermsAndConditions', function (recordset) {
+    return recordset
+  })
+}
+module.exports = getTermsAndConditions()
