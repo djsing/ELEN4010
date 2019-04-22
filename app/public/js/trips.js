653381ec92baf919e3476b8814ad5aac52b0765b
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-04-22 13:14:40 +0200

    addTripTitle() function added and HTML styled

--- app/public/js/trips.js
+++ app/public/js/trips.js
@@ -1,17 +1,8 @@
-let button = document.getElementById('addButton')
 
-// button.addEventListener('click', function () {
-//   let paragraph = document.createElement('p')
-//   let text = document.createTextNode('This is a trip')
-//   paragraph.appendChild(text)
-//   let trips = document.getElementById('tripsList')
-//   trips.appendChild(paragraph)
-// }, false)
-
-button.addEventListener('click', function () {
+let addTripTitle = function () {
   let input = document.createElement('input')
   input.type = 'text'
   input.name = 'tripTitle[]'
   let tripsList = document.getElementById('tripsList')
   tripsList.appendChild(input)
-}, false)
+}
