1cb72b614bef89fe0add1d28bbbad55865530b97
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-04-22 16:09:58 +0200

    Edit and delete buttons with commented functions added

--- app/public/js/trips.js
+++ app/public/js/trips.js
@@ -8,3 +8,28 @@ let addTripTitle = function () {
   document.getElementById('saveButton').hidden = false
   document.getElementById('addButton').disabled = true
 }
+
+// // variable for the user input trip title to be stored to
+
+// let saveTripTitle = function () {
+//   // save the input from the textbox to a variable
+//   // print the value from that variable back to the HTML file (inside the body)
+//   document.getElementById('saveButton').hidden = true
+//   document.getElementById('editButton').hidden = false
+//   document.getElementById('deleteButton').hidden = false
+// }
+
+// let editTripTitle = function () {
+//   // enable the trip title variable to be modified
+//   document.getElementById('saveButton').hidden = false
+//   document.getElementById('editButton').hidden = true
+//   document.getElementById('deleteButton').hidden = true
+// }
+
+// let deleteTripTitle = function () {
+//   // remove the trip title input value from the HTML body
+//   document.getElementById('saveButton').hidden = true
+//   document.getElementById('editButton').hidden = true
+//   document.getElementById('deleteButton').hidden = true
+//   document.getElementById('addButton').disabled = false
+// }
