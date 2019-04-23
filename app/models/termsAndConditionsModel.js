7596c990fdcfbc126620c31ae2a81dd28ddc2b54
Thabang Mabula <1364103@students.wits.ac.za>
2019-04-23 17:58:47 +0200

    Basic display of terms and conditions

--- app/models/termsAndConditionsModel.js
+++ app/models/termsAndConditionsModel.js
@@ -1,10 +1,14 @@
 let db = require('./db.js')
+let termsAndCondtions = [
+  'Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.',
+  'By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.',
+  'By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.',
+  'When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service',
+  'You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.',
+  'You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.'
+]
 
 const getTermsAndConditions = function () {
-  // Get terms and conditions from databasez
-  var request = new db.sql.Request(db.pools)
-  request.query('select * from TermsAndConditions', function (recordset) {
-    return recordset
-  })
+  return termsAndCondtions
 }
 module.exports = getTermsAndConditions()
