e329d0a5da39828297e557276149ae4a3d1bea51
Thabang Mabula <1364103@students.wits.ac.za>
2019-04-23 19:10:07 +0200

    Addition of terms about Links To Other Web Sites

--- app/models/termsAndConditionsModel.js
+++ app/models/termsAndConditionsModel.js
@@ -15,9 +15,18 @@ let Accounts =
     'You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account'
   ]
 }
+let otherSites =
+{
+  name: 'Links To Other Web Sites',
+  terms: [
+    'Our Service may contain links to third-party web sites or services that are not owned or controlled by Away We Go',
+    'Away We Go has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Away We Go shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.',
+    'We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.'
+  ]
+}
 
 let termsAndCondtions = [
-  Accounts
+  Accounts, otherSites
 ]
 
 module.exports = {
