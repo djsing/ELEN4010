bd0103f1f68d642bf221bda79b9591d3ef081a0b
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-04-23 14:53:21 +0200

    Code clean-up

--- app/public/js/trips.js
+++ app/public/js/trips.js
@@ -11,24 +11,10 @@ let addTripTitle = function () {
   document.getElementById('addButton').disabled = true
 }
 
-// // variable for the user input trip title to be stored to
-
 let saveTripTitle = function () {
-  // save the input from the textbox to a variable
-  // print the value from that variable back to the HTML file (inside the body)
   document.getElementById('saveButton').hidden = true
   document.getElementById('editButton').hidden = false
   document.getElementById('deleteButton').hidden = false
-}
-
-// let editTripTitle = function () {
-//   // enable the trip title variable to be modified
-//   document.getElementById('saveButton').hidden = false
-//   document.getElementById('editButton').hidden = true
-//   document.getElementById('deleteButton').hidden = true
-// }
-
-let deleteTripTitle = function (buttonElem) {
 }
 
 let removeRow = function (buttonElem) {
