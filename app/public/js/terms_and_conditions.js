5bbce32b924e21cacb0ebc70b186cb7d4bc49e0a
Thabang Mabula <1364103@students.wits.ac.za>
2019-04-25 18:48:00 +0200

    Ability to itemize terms under subsections

--- app/public/js/terms_and_conditions.js
+++ app/public/js/terms_and_conditions.js
@@ -1,3 +1,14 @@
+let addTermsToSubsection = function (subsection, termsArray) {
+  let list = document.createElement('ol')
+  termsArray.forEach(element => {
+    let item = document.createElement('li')
+    let term = document.createTextNode(element)
+    item.appendChild(term)
+    list.appendChild(item)
+  })
+  subsection.append(list)
+}
+
 $(document).ready(() => {
   let preamble = ''
   let accounts = []
@@ -5,8 +16,7 @@ $(document).ready(() => {
     url: '/terms_and_conditions/data',
     type: 'GET',
     success: (data) => {
-      $('#Preamble').append(data.preamble)
-      $('#Accounts').append(data.accounts)
+      addTermsToSubsection($('#Accounts'), data.accounts)
     }
   })
 })
