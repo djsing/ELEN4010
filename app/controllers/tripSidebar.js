f450ce29bd1cbc3b8a3c98e0a3d5e8e0f49b1e1b
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-04-26 15:30:54 +0200

    Add destinations button adds placeholder values

--- app/controllers/tripSidebar.js
+++ app/controllers/tripSidebar.js
@@ -1,2 +1,17 @@
-// addDestination()
-// saveTrip()
+let saveTripTitle = function () {}
+
+let addDestination = function () {
+  let destinationsTable = document.getElementById('destinationsTableBody')
+  let row = document.createElement('tr')
+  let enumerationCell = document.createElement('td')
+  let enumeration = document.createTextNode('1')
+  enumerationCell.appendChild(enumeration)
+  row.appendChild(enumerationCell)
+  let destinationCell = document.createElement('td')
+  let destination = document.createTextNode('some place')
+  destinationCell.appendChild(destination)
+  row.appendChild(destinationCell)
+  destinationsTable.appendChild(row)
+}
+
+let saveTrip = function () {}
