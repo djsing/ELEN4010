3391692187651e2d7f9d30765a7238b30e910178
Tyson <tyson.cross@gmail.com>
2019-04-30 22:07:06 +0200

    Removed Favicon
    Removed "save-trip" button

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let path = require('path')
 let app = express()
 let mainRouter = require('./app/routes/mainRoutes')
 require('dotenv').config()
-var favicon = require('serve-favicon')
 let bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -15,7 +14,6 @@ app.use(bodyParser.urlencoded({
 app.set('views', path.join(__dirname, './app/views'))
 app.use(express.static(path.join(__dirname, './app/public')))
 app.use(express.static(path.join(__dirname, './app/controllers')))
-app.use(favicon(path.join(__dirname, './app/public/img/favicon.ico')))
 
 app.use('/', mainRouter)
 
