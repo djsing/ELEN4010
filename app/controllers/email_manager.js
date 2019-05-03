7171891d53fb399587bd6723e2ba77941cca32c2
Thabang Mabula <1364103@students.wits.ac.za>
2019-05-03 06:09:21 +0200

    Ability to send an email to a user

---
+++ app/controllers/email_manager.js
@@ -0,0 +1,26 @@
+let fs = require('fs')
+let path = require('path')
+let mailer = require('nodemailer')
+let transporter = mailer.createTransport({
+  host: 'smtp.gmail.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'awaywegoinvites@gmail.com',
+    pass: 'Software3'
+  }
+})
+
+let helperOptions = {
+  from: '"Away We Go" <awaywegoinvites@gmail.com',
+  to: '1364103@students.wits.ac.za',
+  subject: 'Invite to collaborate on a trip',
+  text: 'You have been invited to collaborate on a trip!'
+}
+
+transporter.sendMail(helperOptions, (err, inf) => {
+  if (err) {
+    return console.log(err)
+  }
+  console.log('The email got sent!')
+})
