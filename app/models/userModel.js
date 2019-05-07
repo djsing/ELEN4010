671ac63ed414c4a47465c09f77870c9634101c8c
Tyson <tyson.cross@gmail.com>
2019-05-07 05:24:22 +0200

    Tweaked Anchor
    Adjusted anchor

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -5,7 +5,6 @@ let SqlString = require('sqlstring')
 
 function lookUpUser (req, res) {
   let hash = req.body.hash
-  // let queryString = `SELECT first_name, last_name FROM users WHERE hash = ${hash};`
   let queryString = SqlString.format('SELECT first_name, last_name FROM users WHERE hash = ?;', hash)
   db.getUserName(queryString, res)
 }
