fd93393d33713dd9b135eb9700c7baa670160afd
Darrion Singh <1056673@students.wits.ac.za>
2019-05-17 09:35:56 +0200

    tested groupQuery

--- app/models/__mocks__/db.js
+++ app/models/__mocks__/db.js
@@ -32,7 +32,7 @@ let query = function (queryString) {
         recordset: undefined
       }
       break
-      // Selecting returns results
+    // Selecting returns results
     case 'SELECT id, userId, code, date, importance, trip_id, first_name, last_name FROM log JOIN users ON log.userid = users.hash WHERE trip_id = 0;':
       result = {
         recordset: [{
@@ -45,19 +45,41 @@ let query = function (queryString) {
         }]
       }
       break
+    case 'SELECT * FROM groups WHERE trip_id = 0;':
+      result = {
+        recordset: [{
+          user_hash: 'a1b2c3d4e5f6g7h8i9',
+          trip_id: '123456789'
+        }]
+      }
+      break
+    case 'SELECT * FROM groups WHERE trip_id = 1;':
+      result = {
+        recordset: [{
+          user_hash: 'a1b2c3d4e5f6g7h8i9',
+          trip_id: '123456789'
+        }, {
+          user_hash: 'q1w2e3r4t5y6u7i8o9',
+          trip_id: '987654321'
+        }]
+      }
+      break
 
     default: break
   }
   return new Promise((resolve, reject) => { resolve(result) })
 }
 
-let input = function (str, value) {
+let input = function (str, type, value) {
   this.replacements.push('@' + str)
-  this.values.push(value)
+  if (arguments.length === 2) {
+    this.values.push(type)
+  } else if (arguments.length === 3) { this.values.push(value) }
 }
 
 let pools = new Promise((resolve, reject) => { resolve(new Connection()) })
-
+let sql = { Char: {} }
 module.exports = {
-  pools: pools
+  pools: pools,
+  sql: sql
 }
--- test/log.test.js
+++ test/log.test.js
@@ -1,3 +1,4 @@
+'use strict'
 jest.mock('../app/models/db')
 let logModel = require('../app/models/logModel')
 
