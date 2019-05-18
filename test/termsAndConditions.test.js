f817305e7a875afc3e054722f35b0bcbd5eea98c
Thabang Mabula <1364103@students.wits.ac.za>
2019-05-18 22:50:34 +0200

    Completed test of terms and conditions model

--- test/termsAndConditions.test.js
+++ test/termsAndConditions.test.js
@@ -1,12 +1,30 @@
 let termsAndConditionsModel = require('../app/models/termsAndConditionsModel')
 
-describe('testing the model that supplies the terms and conditions', () => {
-    test('test if the preamble is correct', () => {
+describe('testing the model that exports the correct terms and conditions', () => {
+    test('test if the preamble stord in the model is correct', () => {
         let preamble = [
             'When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.',
             'You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.',
             'You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.'
         ]
-        expect(termsAndConditionsModel.preamble).toBe(preamble)
+        expect(termsAndConditionsModel.getTermsAndCondtions().preamble).toEqual(preamble)
+    })
+
+    test('test if the information stored about accounts is correct', () => {
+        let accounts = [
+            'When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service',
+            'You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service',
+            'You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account'
+        ]
+        expect(termsAndConditionsModel.getTermsAndCondtions().accounts).toEqual(accounts)
+    })
+
+    test('test if the information stored about other sites is correct', () => {
+        let otherSites = [
+            'Our Service may contain links to third-party web sites or services that are not owned or controlled by Away We Go',
+            'Away We Go has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Away We Go shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.',
+            'We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.'
+        ]
+        expect(termsAndConditionsModel.getTermsAndCondtions().otherSites).toEqual(otherSites)
     })
 })
